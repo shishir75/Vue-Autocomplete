995a7b70facf092edf839292a822228d252f10f8
shishir75 <shishir.srdr16@gmail.com>
2020-02-13 01:11:45 +0600

    autocomplete

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="about">
+    <div style="background-color: red;position: absolute; height: 300" @click="model = false"></div>
+
+
     <h1>This is an about page</h1>
 
-    <input type="text" v-model="state" autocomplete="off" @input="filterStates">
+    <input type="text" v-model="state" autocomplete="off" @focus="model = true" >
+
+    <div v-if="filteredStates && model" >
+      <ul>
+        <li v-for="filteredState in filteredStates" v-bind:key="filteredState" @click="setState(filteredState)" style="cursor: pointer">{{ filteredState }}</li>
+      </ul>
+    </div>
 
   </div>
 </template>
@@ -13,6 +22,7 @@
     data: function () {
       return {
         state: '',
+        model: false,
         states: [
                 'Dhaka', 'Rangpur', 'Rajshahi', 'Barishal', 'Chattragam'
         ],
@@ -20,12 +30,30 @@
       }
     },
 
+    mounted() {
+      this.filterStates();
+    },
+
     methods: {
       filterStates() {
+        if (this.state.length == 0) {
+          this.filteredStates = this.states;
+        }
+
         this.filteredStates = this.states.filter(state => {
           return state.toLowerCase().startsWith(this.state.toLowerCase());
         });
+      },
+      setState(state) {
+        this.state = state;
+        this.model = false;
       }
-    }
+    },
+
+    watch: {
+      state() {
+        this.filterStates();
+      }
+    },
   }
 </script>
