edd1064a26723975ed424f437cf4c7ad3d833a12
shishir75 <shishir.srdr16@gmail.com>
2020-02-12 23:58:46 +0600

    autocomplete

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+
+    <input type="text" v-model="state" autocomplete="off" @input="filterStates">
+
   </div>
 </template>
+
+
+<script>
+  export default {
+    data: function () {
+      return {
+        state: '',
+        states: [
+                'Dhaka', 'Rangpur', 'Rajshahi', 'Barishal', 'Chattragam'
+        ],
+        filteredStates: '',
+      }
+    },
+
+    methods: {
+      filterStates() {
+        this.filteredStates = this.states.filter(state => {
+          return state.toLowerCase().startsWith(this.state.toLowerCase());
+        });
+      }
+    }
+  }
+</script>
