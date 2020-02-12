6bab3bd900a5547a6f1b7201804e891ae3455b80
shishir75 <shishir.srdr16@gmail.com>
2020-02-13 01:21:32 +0600

    autocomplete

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="about">
-    <div style="background-color: red;position: absolute; height: 300" @click="model = false"></div>
+    <div style="background-color: red;position: absolute; height: 300px" @click="model = false"></div>
 
 
     <h1>This is an about page</h1>
 
     <input type="text" v-model="state" autocomplete="off" @focus="model = true" >
 
-    <div v-if="filteredStates && model" >
-      <ul>
-        <li v-for="filteredState in filteredStates" v-bind:key="filteredState" @click="setState(filteredState)" style="cursor: pointer">{{ filteredState }}</li>
+    <div v-if="filteredStates && model" style="width: 300px; margin: auto">
+      <ul style="list-style-type: none;">
+        <li v-for="filteredState in filteredStates" v-bind:key="filteredState" @click="setState(filteredState)" style="cursor: pointer; background-color: gray; padding: 5px 10px 5px; margin-top: 2px; color: white">{{ filteredState }}</li>
       </ul>
     </div>
 
